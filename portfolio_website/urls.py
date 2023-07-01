e7438f7f1b787a7b345928e462b757a2df522e31
4KENSENS <kerem1eren@gmail.com>
2023-07-01 22:34:43 +0300

    feat: use ready template

--- portfolio_website/urls.py
+++ portfolio_website/urls.py
@@ -19,9 +19,11 @@ from django.urls import path
 from django.conf.global_settings import DEBUG
 from django.conf import settings
 from django.conf.urls.static import static
+from core.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index,name='index'),
 ]
 
 if settings.DEBUG:
