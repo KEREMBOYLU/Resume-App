5391f35ab3750e21b3baa80ba3071d6d5dcf0f21
4KENSENS <kerem1eren@gmail.com>
2023-07-02 00:32:15 +0300

    refactor: create urls & includes for index and contact

--- portfolio_website/urls.py
+++ portfolio_website/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from django.conf.global_settings import DEBUG
 from django.conf import settings
 from django.conf.urls.static import static
@@ -23,7 +23,7 @@ from core.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index,name='index'),
+    path('', include('core.urls')),
 ]
 
 if settings.DEBUG:
