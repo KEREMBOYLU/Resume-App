887d4028494c7107ee2877d40f8240c15f51ab18
4KENSENS <kerem1eren@gmail.com>
2023-07-03 03:55:05 +0300

    feat: create message model

--- contact/views.py
+++ contact/views.py
@@ -4,10 +4,23 @@ from django.http import JsonResponse
 # Create your views here.
 
 def contact_form(request):
-    context = {
-    'success': True,
-    'message': 'Contact form sent successfully.',
-    }
+    if request.POST:
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+
+        context = {
+            'success': True,
+            'message': 'Contact form sent successfully.',
+        }
+
+    else:
+        context = {
+            'success': False,
+            'message': 'Request method is not valid.',
+        }
+
     return  JsonResponse(context)
 
 def contact(request):
