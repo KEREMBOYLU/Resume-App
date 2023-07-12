62d5f34d953f299181999a5fd16d84ac6932ea63
4KENSENS <kerem1eren@gmail.com>
2023-07-13 02:02:27 +0300

    chore: make frontend changes

--- contact/views.py
+++ contact/views.py
@@ -2,10 +2,44 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from contact.forms import ContactForm
 from contact.models import Message
+from core.models import GeneralSetting, ImageSetting
 
+def get_general_setting(parameter):
+    try:
+        obj = GeneralSetting.objects.get(name=parameter).parameter
+    except:
+        obj = ''
 
+    return obj
+
+def get_image_setting(parameter):
+    try:
+        obj = ImageSetting.objects.get(name=parameter).file
+    except:
+        obj = ''
+
+    return obj
 # Create your views here.
+def layout(request):
 
+    #Contact Infos
+    contact_area_address = get_general_setting('contact_area_address')
+    contact_area_address_under_info = get_general_setting('contact_area_address_underinfo')
+    contact_area_phone = get_general_setting('contact_area_phone')
+    contact_area_phone_under_info = get_general_setting('contact_area_phone_underinfo')
+    contact_area_email = get_general_setting('contact_area_email')
+    contact_area_email_under_info = get_general_setting('contact_area_email_underinfo')
+
+    context = {
+        'contact_area_address': contact_area_address,
+        'contact_area_address_under_info': contact_area_address_under_info,
+        'contact_area_phone': contact_area_phone,
+        'contact_area_phone_under_info': contact_area_phone_under_info,
+        'contact_area_email': contact_area_email,
+        'contact_area_email_under_info': contact_area_email_under_info,
+
+    }
+    return context
 def contact_form(request):
 
     if request.POST:
